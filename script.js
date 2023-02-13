b8b71399b7e5ddece2b19568d9676d1b90cc2e76
meghanlavay <77410138+meghanlavay@users.noreply.github.com>
2023-02-12 21:56:11 -0800

    commit

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // STEP 1
-
 // let someMonth
 // function theMonth()
 // let currentMonth
@@ -7,16 +6,61 @@
 // let mylibraryFunction
 
 // STEP 2
-
 // 5.3     //numeric literal 
 // "hello" //string literal
 // true    //booleanliteral
 // null    //null literal
 
 
-// // STEP 3
-// // Examples of complex/ variable expressions
+// STEP 3
 // let expression_1 = (5+2) * 3;
 // let expression_2 = (price1 + price2);
 
 // STEP 4
+//  let firstName
+//  let strLastName
+//  let strAddress
+//  let strCity
+//  let strState
+//  let zipCode
+//  let intYourAge
+//  let referralSource
+//  let strMayWeContactYou
+
+// STEP 5
+//  let firstName = Meghan
+
+//  let strLastName
+//  strLastName = LaVay
+
+//  const intYourAge
+//  intYourAge = prompt('What is your age?')
+
+// STEP 6
+// let classesCompleted = 3
+// console.log('Meghan has completed ' + classesCompleted + ' classes.');
+
+// STEP 7
+// let var1 = 'to good to be' + true;
+// console.log(var1);
+
+// let var2 = true + 10;
+// console.log(var1);
+
+// STEP 8
+// Not valid. Quotes and apostrophe must be escaped.
+// let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"'
+// console.log(someString)
+
+// STEP 9
+let x = null
+console.log (x)
+
+let x
+console.log(x)
+
+// STEP 10
+
+
+
+
