b0c80232bf73ed7d098efa99bb268d23ce0da59e
meghanlavay <77410138+meghanlavay@users.noreply.github.com>
2023-02-12 18:58:52 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// STEP 1
+
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let mylibraryFunction
+
+// STEP 2
+
+// 5.3     //numeric literal 
+// "hello" //string literal
+// true    //booleanliteral
+// null    //null literal
+
+
+// // STEP 3
+// // Examples of complex/ variable expressions
+// let expression_1 = (5+2) * 3;
+// let expression_2 = (price1 + price2);
+
+// STEP 4
