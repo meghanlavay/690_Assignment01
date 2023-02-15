c22c6039c78ae7dc59f1ad7a3493a775e60ffaf8
meghanlavay <77410138+meghanlavay@users.noreply.github.com>
2023-02-14 16:58:16 -0800

    commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // STEP 1
-// let someMonth
-// function theMonth()
-// let currentMonth
-// let summerMonth
-// let mylibraryFunction
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let mylibraryFunction;
 
 // STEP 2
 // 5.3     //numeric literal 
@@ -17,24 +17,23 @@
 // let expression_2 = (price1 + price2);
 
 // STEP 4
-//  let firstName
-//  let strLastName
-//  let strAddress
-//  let strCity
-//  let strState
-//  let zipCode
-//  let intYourAge
-//  let referralSource
-//  let strMayWeContactYou
+//  let firstName;
+//  let strLastName;
+//  let strAddress;
+//  let strCity;
+//  let strState;
+//  let zipCode;
+//  let intYourAge;
+//  let referralSource;
+//  let strMayWeContactYou;
 
 // STEP 5
-//  let firstName = Meghan
+//  let firstName = 'Meghan';
 
 //  let strLastName;
-//  strLastName = LaVay;
+//  strLastName = 'LaVay';
 
-//  const intYourAge;
-//  intYourAge = prompt('What is your age?');
+//  strCity = 'San Diego', strState = 'CA';
 
 // STEP 6
 // let classesCompleted = 3;
@@ -49,15 +48,15 @@
 
 // STEP 8
 // Not valid. Quotes and apostrophe must be escaped.
-// let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"'
-// console.log(someString)
+// let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+// console.log(someString);
 
 // STEP 9
-// let x = null
-// console.log (x)
+// let x = null;
+// console.log (x);
 
-// let x
-// console.log(x)
+// let x;
+// console.log(x);
 
 // STEP 10
 // console.log(typeof 'Meghan');
@@ -67,9 +66,60 @@
 // console.log(typeof var1);
 
 // STEP 11
+// alert('Hello ' + 'Meghan' + ', welcome to the JavaScript class!');
 
-alert('Hello ' + Meghan + ', welcome to the JavaScript class!');
-
-// STEP 12
-// let myName = Meghan 
+// STEP 12 
+// let myName = 'Meghan LaVay';
 // alert('Hello ' + myName + ', welcome to the JavaScript class!');
+
+// // STEP  13
+// let myName = 'Meghan LaVay';
+// let course = 'JavaScript';
+// alert('Hello ' + myName + ', welcome to the ' + course + ' class!');
+
+// // STEP 14
+// let myName = 'Meghan LaVay';
+// let course = 'JavaScript';
+// alert('Hello ' + myName + '. \nWelcome to the ' + course + ' class!');
+
+// // STEP 15
+// let myName = prompt('What  is your name?');
+// let course = 'JavaScript';
+// alert('Hello ' + myName + '. \nWelcome to the ' + course + ' class!');
+
+// // STEP 16
+// let myName = prompt('What  is your name?');
+// let course = prompt('What class are you taking?');
+// alert('Hello ' + myName + '. \nWelcome to the ' + course + ' class!');
+
+// // STEP 17
+// let x =10;
+// let y = 20;
+// console.log(x + y);
+
+// // STEP 18
+// let x = 20;
+// x+= 20;
+// console.log(x);
+
+// // // STEP19
+// let x = 20;
+// x *= 5;
+// console.log(x);
+
+// // STEP 20
+// x = 20%3;
+// x /= 1;
+// console.log (x);
+
+// // STEP 21
+// x = 5;
+// y =4;
+// console.log(x<10 && y>1);
+
+// // STEP 22
+// x = 5;
+// y = 4;
+// console.log(x==3 || y ==2);
+	
+	
